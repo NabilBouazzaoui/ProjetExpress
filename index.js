0d5dd0ed2ccbe5a898d2560fa1f0f9ab89c7e846
Nabil Bouazzaoui <nabilbouazzaoui62@gmail.com>
2021-03-05 21:30:48 -0500

    projet-1

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express')
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express()
-
-
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
+// API key pk_854cc9f43bbc43e9a2a7d4c7646bfe39
 
-app.get('/', function (req, res) {
-    res.render('home')
-})
-
+app.get('/', function (req, res) { res.render('home') })
+app.get('/about', (req, res) => { res.render('about') })
+app.get('/contact', (req, res) => { res.render('contact') })
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 app.listen(PORT, () => console.log('Server Lintening on port' + PORT))
